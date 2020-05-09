51d500db4c3bfcb0a133af7dc0c3dc3a0c9f9c02
dprojects <dprojects@users.noreply.github.com>
2020-05-09 17:46:51 +0200

    redesign & new index

--- js/actions.js
+++ js/actions.js
@@ -1,33 +1,19 @@
-var output = "";
-var period = 14;
+/* ----------------------------------------------------------------------------------------------------------------------------
+   GLOBALS
+   ---------------------------------------------------------------------------------------------------------------------------- */
 
-var tTable1 = '<table id="covid-table-1" class="display">';
-var tTable2 = '<table id="covid-table-2" class="display">';
-var tTable3 = '<table id="covid-table-3" class="display">';
-var tTable4 = '<table id="covid-table-4" class="display">';
+var tPanel = '';
+var tTitle = '';
+var output = '';
 
-// header for table 1,2,3
-var tHead = '<thead>';
-tHead += '<tr>';
-tHead += '<th class="col1">Place</th>';
-tHead += '<th class="col2">Confirmed</th>';
-tHead += '<th class="col3">Deaths</th>';
-tHead += '<th class="col4">Deaths %</th>';
-tHead += '<th class="col5">Recovered</th>';
-tHead += '<th class="col6">Recovered %</th>';
-tHead += '</tr>';
-tHead += '</thead>';
-
-// header for table 4
-var tHead4 = '<thead>';
-tHead4 += '<tr>';
-tHead4 += '<th class="col1">Place</th>';
-tHead4 += '<th class="col2">Confirmed</th>';
-tHead4 += '<th class="col3">Trend index</th>';
-tHead4 += '</tr>';
-tHead4 += '</thead>';
+var period = 7;
+var table = "";
 
 var lastUpdate = '';
+
+/* ----------------------------------------------------------------------------------------------------------------------------
+   CALCULATIONS
+   ---------------------------------------------------------------------------------------------------------------------------- */
 
 function getTrend(key, data, p) {
     
@@ -53,162 +39,148 @@ function getTrend(key, data, p) {
     return tIndex;
 }
 
+function getGrow(key, data, p) {
+    
+    let tGrow = 0;
+    let size = data.length-1;
+     
+    for(let i=0; i<p; i++) {
+        
+        let id = size-i;
+        tGrow += ( (data[id].confirmed - data[id-1].confirmed) / data[id].confirmed ) * 100;
+    }
+    return tGrow/p;
+}
+
+/* ----------------------------------------------------------------------------------------------------------------------------
+   BUTTONS
+   ---------------------------------------------------------------------------------------------------------------------------- */
+function showDeaths(){
+    table.order([3, 'asc']).draw();
+    tTitle = 'Best covid-19 healthcare';
+    document.getElementById("TITLE").innerHTML = tTitle;
+}
+function showRecov(){
+    table.order([5, 'dsc']).draw();
+    tTitle = 'Best covid-19 population';
+    document.getElementById("TITLE").innerHTML = tTitle;
+}
+function showConfirm(){
+    table.order([1, 'asc']).draw();
+    tTitle = 'Best covid-19 politics';
+    document.getElementById("TITLE").innerHTML = tTitle;
+}
+function showTrend(){
+    table.order([6, 'asc']).draw();
+    tTitle = 'Last '+period+' days covid-19 trend';
+    document.getElementById("TITLE").innerHTML = tTitle;
+}
+function showGrow(){
+    table.order([7, 'asc']).draw();
+    tTitle = 'Last '+period+' days covid-19 grow';
+    document.getElementById("TITLE").innerHTML = tTitle;
+}
+
+/* ----------------------------------------------------------------------------------------------------------------------------
+   GET DATA & BUILD PAGE
+   ---------------------------------------------------------------------------------------------------------------------------- */
 
 fetch("https://pomber.github.io/covid19/timeseries.json")
   .then(response => response.json())
   .then(data => {
 
-    var tBody1 = '<tbody>';
-    var tBody2 = '<tbody>';
-    var tBody3 = '<tbody>';
-    var tBody4 = '<tbody>';
-
+    var tBody = '<tbody>';
+    
     Object.keys(data).forEach(function(key) {
 
-        var size = data[key].length-1;
+        let size = data[key].length-1;
 
-        var col1 = key;
-        var col2 = data[key][size].confirmed;
-        var col3 = data[key][size].deaths;
+        let col1 = key;
+        let col2 = data[key][size].confirmed;
+        let col3 = data[key][size].deaths;
 
-        var col4 = 0;
+        let col4 = 0;
         if (col2 !== 0) {
             col4 = ( col3 / col2 ) * 100;
         }
 
-        var col5 = data[key][size].recovered;
+        let col5 = data[key][size].recovered;
 
-        var col6 = 0;
+        let col6 = 0;
         if (col2 !== 0) {
             col6 = ( col5 / col2 ) * 100;
         }
-
-        // build content for table 1
-        if (col2 > 100) {
-
-            tBody1 += '<tr>';
-
-            tBody1 += '<td class="col1">' + col1 + '</td>';
-            tBody1 += '<td class="col2">' + col2 + '</td>';
-            tBody1 += '<td class="col3">' + col3 + '</td>';
-            tBody1 += '<td class="col4">' + col4.toFixed(2) + '</td>';
-            tBody1 += '<td class="col5">' + col5 + '</td>';
-            tBody1 += '<td class="col6">' + col6.toFixed(2) + '</td>';
-
-            tBody1 += '</tr>';
-        }
-
-        // build content for table 2
-        if (col2 > 100) {
-
-            tBody2 += '<tr>';
-
-            tBody2 += '<td>' + col1 + '</td>';
-            tBody2 += '<td>' + col2 + '</td>';
-            tBody2 += '<td>' + col3 + '</td>';
-            tBody2 += '<td>' + col4.toFixed(2) + '</td>';
-            tBody2 += '<td>' + col5 + '</td>';
-            tBody2 += '<td>' + col6.toFixed(2) + '</td>';
-
-            tBody2 += '</tr>';
-        }
-
-        // build content for table 3
-        //if (col2 > 10000) {
-
-            tBody3 += '<tr>';
-
-            tBody3 += '<td>' + col1 + '</td>';
-            tBody3 += '<td>' + col2 + '</td>';
-            tBody3 += '<td>' + col3 + '</td>';
-            tBody3 += '<td>' + col4.toFixed(2) + '</td>';
-            tBody3 += '<td>' + col5 + '</td>';
-            tBody3 += '<td>' + col6.toFixed(2) + '</td>';
-
-            tBody3 += '</tr>';
-        //}
-
-        // build content for table 4
-        let tIndex = getTrend(key, data[key], period);    
-        if (tIndex !== 0) {
-
-            tBody4 += '<tr>';
-
-            tBody4 += '<td>' + col1 + '</td>';
-            tBody4 += '<td>' + col2 + '</td>';
-            tBody4 += '<td>' + tIndex.toFixed(2) + '</td>';
-
-            tBody4 += '</tr>';
-        }    
+        
+        let col7 = getTrend(key, data[key], period);
+        let col8 = getGrow(key, data[key], period);
+        
+        // build table content
+        tBody += '<tr>';
+        tBody += '<td class="col1">' + col1 + '</td>';
+        tBody += '<td class="col2">' + col2 + '</td>';
+        tBody += '<td class="col3">' + col3 + '</td>';
+        tBody += '<td class="col4">' + col4.toFixed(2) + '</td>';
+        tBody += '<td class="col5">' + col5 + '</td>';
+        tBody += '<td class="col6">' + col6.toFixed(2) + '</td>';
+        tBody += '<td class="col7">' + col7 + '</td>';
+        tBody += '<td class="col8">' + col8.toFixed(2) + '</td>';
+        tBody += '</tr>';
             
         lastUpdate = data[key][size].date;
     });
 
-    tBody1 += '</tbody>';
-    tBody2 += '</tbody>';
-    tBody3 += '</tbody>';
-    tBody4 += '</tbody>';
+    tBody += '</tbody>';
+    
+    // header for table
+    var tHead = '<thead>';
+    tHead += '<tr>';
+    tHead += '<th class="col1">Place</th>';
+    tHead += '<th class="col2">Confirmed</th>';
+    tHead += '<th class="col3">Deaths</th>';
+    tHead += '<th class="col4">Deaths %</th>';
+    tHead += '<th class="col5">Recovered</th>';
+    tHead += '<th class="col6">Recovered %</th>';
+    tHead += '<th class="col7">Trend</th>';
+    tHead += '<th class="col8">Grow %</th>';
+    tHead += '</tr>';
+    tHead += '</thead>';
 
     // PAGE CONTENT
-    output += '<div class="box-page">';
-
+    
+    // panel section
+    tPanel += '<button onClick="showDeaths()">show</button> - best covid-19 healthcare (sort by Deaths % - it is default view)'+'</br>';
+    tPanel += '<button onClick="showRecov()">show</button> - best covid-19 population resistent (sort by Recovered %)'+'</br>';
+    tPanel += '<button onClick="showConfirm()">show</button> - best covid-19 politics (sort by Confirmed)'+'</br>';
+    tPanel += '<button onClick="showTrend()">show</button> - sort by trend index (lower value is better, there is more daily confirmed small down trends during this period than jumps)'+'</br>';
+    tPanel += '<button onClick="showGrow()">show</button> - sort by grow index (lower value is better, means average of confirmed % during this period is not so big)'+'</br>';
+    document.getElementById("PANEL").innerHTML = tPanel;
+    
+    // title section 
+    tTitle += 'Best covid-19 healthcare';
+    document.getElementById("TITLE").innerHTML = tTitle;
+    
+    // just the table output section
     output += '<div class="box-table">';
-        output += '<div class="title-table">Best covid-19 healthcare</div>';
-        output += tTable1 + tHead + tBody1 + '</table>';
-    output += '</div>';
-
-    output += '<div class="box-table">';
-        output += '<div class="title-table">Best covid-19 population resistent</div>';
-        output += tTable2 + tHead + tBody2 + '</table>';
-    output += '</div>';
-
-    output += '<div class="box-table">';
-        output += '<div class="title-table">Best covid-19 politics</div>';
-        output += tTable3 + tHead + tBody3 + '</table>';
-    output += '</div>';
-
-    output += '<div class="box-table">';
-        output += '<div class="title-table">Last '+period+' days covid-19 trend</div>';
-        output += tTable4 + tHead4 + tBody4 + '</table>';
+        output += '<table id="covid-table" class="display">';
+        output += tHead + tBody;
+        output += '</table>';
     output += '</div>';
     
+    // update status info
     output += '<div class="bg-info">Last update: ' + lastUpdate + '</div>';
 
-    output += '</div>';
-
+    // show the output part
     document.getElementById("OUTPUT").innerHTML = output;
-
+    
+    // show the table
     $(document).ready( function () {
-
-        var table1 = $('#covid-table-1').DataTable( {
+        table = $('#covid-table').DataTable( {
             "order": [[ 3, 'asc' ]],
             "orderClasses": true,
             responsive: true
         } );
-
-        var table2 = $('#covid-table-2').DataTable( {
-            "order": [[ 5, 'dsc' ]],
-            "orderClasses": true,
-            responsive: true
-        } );
-
-        var table3 = $('#covid-table-3').DataTable( {
-            "order": [[ 1, 'asc' ]],
-            "orderClasses": true,
-            responsive: true
-        } );
-        
-        var table4 = $('#covid-table-4').DataTable( {
-            "order": [[ 2, 'asc' ]],
-            "orderClasses": true,
-            responsive: true
-        } );
-
-        new $.fn.dataTable.FixedHeader( table1 );
-        new $.fn.dataTable.FixedHeader( table2 );
-        new $.fn.dataTable.FixedHeader( table3 );
-        new $.fn.dataTable.FixedHeader( table4 );
-
+        new $.fn.dataTable.FixedHeader( table );        
     } );
-
+    
   });
+  
