1a0cd85b7efc2579b83490c881f32424263b9072
dprojects <dprojects@users.noreply.github.com>
2020-05-09 21:55:22 +0200

    custom period

--- js/actions.js
+++ js/actions.js
@@ -2,14 +2,14 @@
    GLOBALS
    ---------------------------------------------------------------------------------------------------------------------------- */
 
-var tPanel = '';
 var tTitle = '';
 var output = '';
 
 var period = 7;
-var table = "";
+var table = '';
 
-var lastUpdate = '';
+var aData = '';
+var aSize = 0;
 
 /* ----------------------------------------------------------------------------------------------------------------------------
    CALCULATIONS
@@ -47,7 +47,12 @@ function getGrow(key, data, p) {
     for(let i=0; i<p; i++) {
         
         let id = size-i;
-        tGrow += ( (data[id].confirmed - data[id-1].confirmed) / data[id].confirmed ) * 100;
+        
+        if (data[id].confirmed === 0) {
+            tGrow += 0;
+        } else {
+            tGrow += ( (data[id].confirmed - data[id-1].confirmed) / data[id].confirmed ) * 100;
+        }
     }
     return tGrow/p;
 }
@@ -55,56 +60,105 @@ function getGrow(key, data, p) {
 /* ----------------------------------------------------------------------------------------------------------------------------
    BUTTONS
    ---------------------------------------------------------------------------------------------------------------------------- */
-function showDeaths(){
+
+function showDeaths() {
+    
     table.order([3, 'asc']).draw();
     tTitle = 'Best covid-19 healthcare';
     document.getElementById("TITLE").innerHTML = tTitle;
 }
-function showRecov(){
+
+function showRecov() {
+    
     table.order([5, 'dsc']).draw();
     tTitle = 'Best covid-19 population';
     document.getElementById("TITLE").innerHTML = tTitle;
 }
-function showConfirm(){
+
+function showConfirm() {
+    
     table.order([1, 'asc']).draw();
     tTitle = 'Best covid-19 politics';
     document.getElementById("TITLE").innerHTML = tTitle;
 }
-function showTrend(){
+
+function showTrend() {
+    
     table.order([6, 'asc']).draw();
     tTitle = 'Last '+period+' days covid-19 trend';
     document.getElementById("TITLE").innerHTML = tTitle;
 }
-function showGrow(){
+
+function showGrow() {
+    
     table.order([7, 'asc']).draw();
     tTitle = 'Last '+period+' days covid-19 grow';
     document.getElementById("TITLE").innerHTML = tTitle;
 }
 
 /* ----------------------------------------------------------------------------------------------------------------------------
-   GET DATA & BUILD PAGE
+   SET PAGE
    ---------------------------------------------------------------------------------------------------------------------------- */
 
-fetch("https://pomber.github.io/covid19/timeseries.json")
-  .then(response => response.json())
-  .then(data => {
+function setPanel() {
+    
+    let tPanel = '';
+    
+    tPanel += 'Period: ';
+    tPanel += '<input type="radio" id="period1" name="period" value="7" checked="checked" onClick="setPeriod(7)" />';
+    tPanel += '<label for="7">7 days</label>';
+    tPanel += '&nbsp;&nbsp;';
+    tPanel += '<input type="radio" id="period2" name="period" value="14"  onClick="setPeriod(14)" />';
+    tPanel += '<label for="14">14 days</label>';
+    tPanel += '&nbsp;&nbsp;';
+    tPanel += '<input type="radio" id="period3" name="period" value="0"  onClick="setPeriod(0)" />';
+    tPanel += '<label for="0">all available days</label>';
+    tPanel += '</br>';
+    
+    tPanel += '<button onClick="showDeaths()">show</button>';
+    tPanel += ' - best covid-19 healthcare (sort by Deaths % - it is default view)';
+    tPanel += '</br>';
+    tPanel += '<button onClick="showRecov()">show</button>';
+    tPanel += ' - best covid-19 population resistent (sort by Recovered %)';
+    tPanel += '</br>';
+    tPanel += '<button onClick="showConfirm()">show</button>';
+    tPanel += ' - best covid-19 politics (sort by Confirmed)';
+    tPanel += '</br>';
+    tPanel += '<button onClick="showTrend()">show</button>';
+    tPanel += ' - sort by trend index (lower value is better, there is more daily confirmed ';
+    tPanel += 'small down trends during this period than jumps)';
+    tPanel += '</br>';
+    tPanel += '<button onClick="showGrow()">show</button>';
+    tPanel += ' - sort by grow index (lower value is better, means average of confirmed % during this period is not so big)';
+    tPanel += '</br>';
+    
+    document.getElementById("PANEL").innerHTML = tPanel;
+}
 
-    var tBody = '<tbody>';
+function setTitle() {
+    
+    tTitle = 'Best covid-19 healthcare';
+    document.getElementById("TITLE").innerHTML = tTitle;
+}
+
+function setTable(data) {
+    
+    let tBody = '<tbody>';
     
     Object.keys(data).forEach(function(key) {
 
-        let size = data[key].length-1;
+        aSize = data[key].length-1;
 
         let col1 = key;
-        let col2 = data[key][size].confirmed;
-        let col3 = data[key][size].deaths;
+        let col2 = data[key][aSize].confirmed;
+        let col3 = data[key][aSize].deaths;
 
         let col4 = 0;
         if (col2 !== 0) {
             col4 = ( col3 / col2 ) * 100;
         }
 
-        let col5 = data[key][size].recovered;
+        let col5 = data[key][aSize].recovered;
 
         let col6 = 0;
         if (col2 !== 0) {
@@ -125,14 +179,12 @@ fetch("https://pomber.github.io/covid19/timeseries.json")
         tBody += '<td class="col7">' + col7 + '</td>';
         tBody += '<td class="col8">' + col8.toFixed(2) + '</td>';
         tBody += '</tr>';
-            
-        lastUpdate = data[key][size].date;
     });
 
     tBody += '</tbody>';
     
     // header for table
-    var tHead = '<thead>';
+    let tHead = '<thead>';
     tHead += '<tr>';
     tHead += '<th class="col1">Place</th>';
     tHead += '<th class="col2">Confirmed</th>';
@@ -145,33 +197,63 @@ fetch("https://pomber.github.io/covid19/timeseries.json")
     tHead += '</tr>';
     tHead += '</thead>';
 
-    // PAGE CONTENT
-    
-    // panel section
-    tPanel += '<button onClick="showDeaths()">show</button> - best covid-19 healthcare (sort by Deaths % - it is default view)'+'</br>';
-    tPanel += '<button onClick="showRecov()">show</button> - best covid-19 population resistent (sort by Recovered %)'+'</br>';
-    tPanel += '<button onClick="showConfirm()">show</button> - best covid-19 politics (sort by Confirmed)'+'</br>';
-    tPanel += '<button onClick="showTrend()">show</button> - sort by trend index (lower value is better, there is more daily confirmed small down trends during this period than jumps)'+'</br>';
-    tPanel += '<button onClick="showGrow()">show</button> - sort by grow index (lower value is better, means average of confirmed % during this period is not so big)'+'</br>';
-    document.getElementById("PANEL").innerHTML = tPanel;
-    
-    // title section 
-    tTitle += 'Best covid-19 healthcare';
-    document.getElementById("TITLE").innerHTML = tTitle;
-    
-    // just the table output section
-    output += '<div class="box-table">';
+    output = '<div class="box-table">';
         output += '<table id="covid-table" class="display">';
         output += tHead + tBody;
         output += '</table>';
     output += '</div>';
-    
-    // update status info
-    output += '<div class="bg-info">Last update: ' + lastUpdate + '</div>';
+}
 
-    // show the output part
-    document.getElementById("OUTPUT").innerHTML = output;
+function setStatus(data) {
     
+    let key = Object.keys(data)[0];
+    let s = data[key].length-1;
+    
+    lastUpdate = data[key][s].date;
+    output += '<div class="bg-info">Last update: ' + lastUpdate + '</div>';
+}
+
+function setOutput(data) {
+
+    setTable(data);  
+    setStatus(data);   
+
+    document.getElementById("OUTPUT").innerHTML = output;
+}
+
+function setPeriod(p) {
+    
+    if (p === 0) { period = aSize; } else { period = p; }
+        
+    setTitle();
+    setOutput(aData);    
+    
+    table.destroy();
+    
+    table = $('#covid-table').DataTable( {
+        "order": [[ 3, 'asc' ]],
+        "orderClasses": true,
+        responsive: true
+    } );
+    new $.fn.dataTable.FixedHeader( table );        
+}
+
+/* ----------------------------------------------------------------------------------------------------------------------------
+   GET DATA & BUILD INITIAL PAGE
+   ---------------------------------------------------------------------------------------------------------------------------- */
+
+fetch("https://pomber.github.io/covid19/timeseries.json")
+  .then(response => response.json())
+  .then(data => {
+    
+    // make backup for update output section purposes
+    aData = data;
+    
+    // INITIAL PAGE CONTENT
+    setPanel();
+    setTitle(); 
+    setOutput(data);   
+
     // show the table
     $(document).ready( function () {
         table = $('#covid-table').DataTable( {
@@ -181,6 +263,5 @@ fetch("https://pomber.github.io/covid19/timeseries.json")
         } );
         new $.fn.dataTable.FixedHeader( table );        
     } );
-    
-  });
+});
   
